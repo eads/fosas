3ae6ff6dba8005529737f63acb7fd183bb5ea7a1
David Eads <davideads@gmail.com>
2014-04-05 10:12:32 -0600

    Created fosas from https://github.com/newsapps/tarbell-template

---
+++ tarbell_config.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tarbell project configuration
+"""
+
+# Short project name
+NAME = "fosas"
+
+# Descriptive title of project
+TITLE = "Fosas"
+
+# Google spreadsheet key
+SPREADSHEET_KEY = "0Ak3IIavLYTovdHUybFZleWdTTGNVM3ZZb05rWmJ6WlE"
+
+# Exclude these files from publication
+EXCLUDES = ["*.md", "requirements.txt"]
+
+# Create JSON data at ./data.json, disabled by default
+# CREATE_JSON = True
+
+# S3 bucket configuration
+S3_BUCKETS = {
+    # Provide target -> s3 url pairs, such as:
+    #     "mytarget": "mys3url.bucket.url/some/path"
+    # then use tarbell publish mytarget to publish to it
+    "production": "apps.chicagotribune.com/fosas",
+    "staging": "apps.beta.tribapps.com/fosas",
+}
+
+# Default template variables
+DEFAULT_CONTEXT = {
+    'name': 'fosas',
+    'title': 'Fosas'
+}
